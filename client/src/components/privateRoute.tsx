6b04b91685ab5c101c7cc7c0626c500cd203471b
bunnygarg11 <57141572+bunnygarg11@users.noreply.github.com>
2021-06-28 13:48:00 +0530

    formatted

--- client/src/components/privateRoute.tsx
+++ client/src/components/privateRoute.tsx
@@ -5,44 +5,41 @@ import { IRootState } from "../types/interfaces";
 import { UserType } from "../types/enum";
 
 export type ProtectedRouteProps = {
-	isAuthenticated: boolean;
-	path: string;
-	userType?: UserType.ADMIN | UserType.USER;
-	component: React.ComponentType<any>;
-	[propName: string]: any;
+  isAuthenticated: boolean;
+  path: string;
+  userType?: UserType.ADMIN | UserType.USER;
+  component: React.ComponentType<any>;
+  [propName: string]: any;
 } & RouteProps;
 
 const config = {
-	[UserType.ADMIN]: ["/adminDashboard", "/userDashboard"],
-	[UserType.USER]: ["/userDashboard"],
+  [UserType.ADMIN]: ["/adminDashboard", "/userDashboard"],
+  [UserType.USER]: ["/userDashboard"],
 };
 
 const PrivateRoute: React.FC<ProtectedRouteProps> = ({
-	component: Component,
-	isAuthenticated,
-	path,
-	userType,
-	...rest
+  component: Component,
+  isAuthenticated,
+  path,
+  userType,
+  ...rest
 }) => {
-	console.log('====================================');
-	console.log(isAuthenticated, path, userType);
-	console.log('====================================');
-	return (
-		<Route
-			{...rest}
-			render={(props) =>
-				!isAuthenticated || !(userType && config[userType].includes(path)) ? (
-					<Redirect to="/" />
-				) : (
-					<Component {...props} />
-				)
-			}
-		/>
-	);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !isAuthenticated || !(userType && config[userType].includes(path)) ? (
+          <Redirect to="/" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
 };
 
 const mapStatetoProps = (state: IRootState) => ({
-	isAuthenticated: state.auth.isAuthenticated,
-	userType: state.auth.user?.userType,
+  isAuthenticated: state.auth.isAuthenticated,
+  userType: state.auth.user?.userType,
 });
 export default connect(mapStatetoProps)(PrivateRoute);
