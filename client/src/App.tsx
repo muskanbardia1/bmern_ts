63de08a2740437c11cbaeae643d6d41a231f9e53
bunnygarg11 <57141572+bunnygarg11@users.noreply.github.com>
2021-07-02 19:25:58 +0530

    done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,40 +7,41 @@ import { Router, Route, Switch } from "react-router-dom";
 import UserDashboard from "./components/UserDashboard";
 import AdminDashboard from "./components/AdminDashboard";
 import { getUserById } from "./flux/actions/authActions";
-import { useDispatch } from "react-redux"
-import ProtectedRoute from "./components/privateRoute"
-
+import { useDispatch } from "react-redux";
+import ProtectedRoute from "./components/privateRoute";
 
 import "./App.css";
 export const history = createBrowserHistory();
 
 function App() {
-	const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getUserById());
-	}, []);
-	return (
-		<Router history={history}>
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    token && dispatch(getUserById());
+  }, []);
+  return (
+    <Router history={history}>
       <div className="App">
-        <AlertBar/>
-				<Switch>
-					<Route path="/" exact component={SignIn} />
-					<Route exact path="/register" component={Register} />
-					<ProtectedRoute
-						exact
-						path="/userDashboard"
-						component={UserDashboard}
-					/>
-					<ProtectedRoute
-						exact
-						path="/adminDashboard"
-						component={AdminDashboard}
-					/>
-				</Switch>
-			</div>
-		</Router>
-	);
+        <AlertBar />
+        <Switch>
+          <Route path="/" exact component={SignIn} />
+          <Route exact path="/register" component={Register} />
+          <ProtectedRoute
+            exact
+            path="/userDashboard"
+            component={UserDashboard}
+          />
+          <ProtectedRoute
+            exact
+            path="/adminDashboard"
+            component={AdminDashboard}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
